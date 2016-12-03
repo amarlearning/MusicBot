27c89721563abf66cff5d2dc8d8fd2c054197e04
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2016-12-04 04:26:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,91 @@
 <?php
 
-    $hubVerifyToken = 'xxxxxxxxxxxxxxx';
-    $accessToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	// Necessary Token Variables for API access!
+	
+	$hubVerifyToken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+	$accessToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	$youtube_api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	
+	
+	$regex = "/(hello)|(hi)/";
 
-    if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-      echo $_REQUEST['hub_challenge'];
-      exit;
-    }
+	if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+	  echo $_REQUEST['hub_challenge'];
+	  exit;
+	}
 
-    $input = json_decode(file_get_contents('php://input'), true);
+	$input = json_decode(file_get_contents('php://input'), true);
+	
+	$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+	$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+	
+	$answer = "I don't understand.";
+	
+	$url = 'https://graph.facebook.com/v2.8/'.$senderId.'?access_token='. $accessToken;
+	$context = stream_context_create(array(
+	  'http' => array(
+		 'ignore_errors'=>true,
+		 'method'=>'GET'
+	   )
+	));
+	
+	$response = json_decode(file_get_contents($url, false, $context), true);
+	
+	if(preg_match($regex, $messageText) && $hello_flag == 0) {
+		// $answer = "Hello ". $response['first_name'] . "!\n\nWhat can this bot do ?\nI have been programmed to make your life simple, by sending you your favourite song!\n\nYou just have to send the name of song that you want to download and i will send you that song as an audio message!";
+	}
     
-    $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
-    $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-    
-    $answer = "I don't understand. Ask me 'hi'.";
-    
-    if($messageText == "hi") {
-        $answer = "Hello";
-    }
-    
-    $response = [
+    $url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=". urlencode($messageText) . "&key=" . $youtube_api_key;
+	$context = stream_context_create(array(
+	  'http' => array(
+		 'ignore_errors'=>true,
+		 'method'=>'GET'
+	   )
+	));
+	
+	$video_result_data = json_decode(file_get_contents($url, false, $context), true);
+	
+	// $video_id = $video_result_data['items'][0]['id']['videoId'];
+	$count = 0;
+	while(empty($video_result_data['items'][$count]['id']['videoId'])) {
+		$count++;
+	}
+	
+	$video_id = $video_result_data['items'][$count]['id']['videoId'];
+	$video_name = $video_result_data['items'][$count]['snippet']['title'];
+
+	$base_url = "https://www.youtubeinmp3.com/fetch/?format=JSON&video=https://www.youtube.com/watch?v=". $video_id;
+	$download_url = json_decode(file_get_contents($base_url, false, $context), true);
+
+	$response = [
         'recipient' => [
-            'id' => $senderId 
+            'id' => $senderId
             ],
         'message' => [ 
-            'text' => $answer 
+                "attachment" => [
+                    "type"=>"template",
+                    "payload"=> [
+						"template_type" => "button",
+                        "text" => "Here's your song! 🎼🎧\n". $video_name,
+						"buttons" => [[
+							"type" => "web_url",
+							"url" => "http://amarpandey.me/",
+							"title" => "Download Song"
+					]]
+                ]
             ]
+        ]
     ];
-    
-    $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-    
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-    
-    if(!empty($input['entry'][0]['messaging'][0]['message'])) {
-        curl_exec($ch);
-    }
-    curl_close($ch);
+	
+	// echo $download_url['link'];
+	
+	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+	
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+	
+	if(!empty($input['entry'][0]['messaging'][0]['message'])) {
+		curl_exec($ch);
+	}
+	curl_close($ch);
